77e3fe6bb8b72a57b78387d4eabb630083fb9dae
tomaszbilka <bila4@op.pl>
2024-01-30 07:23:52 +0100

    add colors and spacings

---
+++ components/styles/colors.ts
@@ -0,0 +1,12 @@
+const black = "#000000";
+const white = "#FFFFFF";
+
+const colors = {
+  black,
+  white,
+
+  text: {
+    primary: white,
+    secondary: black,
+  },
+};
---
+++ components/styles/spacing.ts
@@ -0,0 +1,13 @@
+const xsBase = 8
+const base = xsBase * 3
+
+export const padding = {
+  xxs: xsBase / 2, //4
+  xs: xsBase, // 8
+  sm: xsBase * 2, // 16
+  base: xsBase * 3, // 24
+  md: xsBase * 4, // 32
+  lg: base * 2, // 48
+  xl: base * 3, // 72
+  xll: base * 4, // 96
+}
